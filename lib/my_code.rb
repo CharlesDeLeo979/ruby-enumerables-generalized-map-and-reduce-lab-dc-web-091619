a60dc915fd1d18ea0f0113163b977866d4135432
Charles DeLeo <charles.deleo979@gmail.com>
2019-09-06 18:33:24 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -2,7 +2,7 @@ def map_to_negativize(source_array)
   new = []
   i = 0
   while i < source_array.length do
-    new.push( source_array[i] * -1 ) # <== Unique work
+    new.push( yield(array[i] * -1)) # <== Unique work
     i += 1
   end
   return new
