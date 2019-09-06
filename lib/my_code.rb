43e107ca3d2e61d8776d83d185d84a73be87fed0
Charles DeLeo <charles.deleo979@gmail.com>
2019-09-06 18:34:35 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -2,8 +2,8 @@ def map(array)
   new = []
   i = 0
   while i < source_array.length do
-    new.push( yield(array[i])) # <== Unique work
-    i += 1
+    new.push( yield(array[i])) 
+    i += -1
   end
   return new
 end
